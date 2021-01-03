6f61105fb3a254984e19f02673e27452d8b5aa05
Samuel Bran <hello@samuelbran.com>
2021-01-03 17:11:07 -0600

    Fix the domain and... year

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="bg-gray-50 text-center">
       <div className="p-12">
-        <p>samuelbran.com - 2010</p>
+        <p>bran.today - 2021</p>
         <p>Website built using Next.js and Tailwind CSS</p>
       </div>
     </footer>
